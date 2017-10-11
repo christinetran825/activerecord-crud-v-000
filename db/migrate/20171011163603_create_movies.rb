a3be7b4b645d39c080d66af2f3fc8c7aa750d53a
Christine Tran <christinetran825@gmail.com>
2017-10-11 21:19:19 +0000

    automatically backed up by learn

--- db/migrate/20171011163603_create_movies.rb
+++ db/migrate/20171011163603_create_movies.rb
@@ -1,9 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    add_columns :movie, :title, :string
-    add_columns :movie, :release_date, :integer
-    add_columns :movie, :director, :string
-    add_columns :movie, :lead, :string
-    add_columns :movie, :in_theaters, :boolean
+    
   end
 end
+
+# add_columns :movie, :title, :string
+# add_columns :movie, :release_date, :integer
+# add_columns :movie, :director, :string
+# add_columns :movie, :lead, :string
+# add_columns :movie, :in_theaters, :boolean
